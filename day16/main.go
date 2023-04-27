234f9e5081f938e9030523c49b71f81bf62ce868
Marc Dominik Migge <marcmigge@gmx.net>
2023-04-27 18:07:29 +0200

    Finish day 16

--- day16/main.go
+++ day16/main.go
@@ -12,9 +12,15 @@ import (
 )
 
 type valve struct {
-	id      string
-	flow    int
-	tunnels []string
+	id        string
+	flow      int
+	tunnels   []string
+	distances map[string]int
+}
+
+type action struct {
+	move     bool
+	location string
 }
 
 func parse() (valves map[string]valve) {
@@ -43,7 +49,7 @@ func parse() (valves map[string]valve) {
 
 		var tunnels = strings.Split(match[3], ", ")
 
-		valves[id] = valve{id, flow, tunnels}
+		valves[id] = valve{id, flow, tunnels, nil}
 	}
 
 	if err := scanner.Err(); err != nil {
@@ -70,7 +76,74 @@ func parse() (valves map[string]valve) {
 	return valves
 }
 
-func stateHash(location string, minutes int, open map[string]int) string {
+func findDistances(valves map[string]valve, start string) map[string]int {
+	visited := make(map[string]bool)
+	steps := make(map[string]int)
+	for v, _ := range valves {
+		visited[v] = false
+		steps[v] = 0
+	}
+
+	queue := []string{start}
+	visited[start] = true
+	for len(queue) > 0 {
+		current := queue[0]
+		queue = queue[1:]
+
+		for _, next := range valves[current].tunnels {
+			if !visited[next] {
+				queue = append(queue, next)
+				visited[next] = true
+				steps[next] = steps[current] + 1
+			}
+		}
+
+	}
+
+	return steps
+}
+
+func mapNetwork(valves map[string]valve) map[string]valve {
+	mappedValves := make(map[string]valve)
+	for id, v := range valves {
+		mappedValves[id] = valve{id, v.flow, v.tunnels, findDistances(valves, id)}
+	}
+
+	return mappedValves
+}
+
+func findMaxFlow(valves map[string]valve, location string, open map[string]int, minutes int, limit int, memo map[string]int) int {
+	max := 0
+	progress := false
+	for next, _ := range valves {
+		_, opened := open[next]
+		if !opened && valves[next].flow > 0 {
+			eta := minutes + valves[location].distances[next] + 1
+			if eta <= limit {
+				progress = true
+				open[next] = eta
+				flow := findMaxFlow(valves, next, open, eta, limit, memo)
+				delete(open, next)
+
+				if flow > max {
+					max = flow
+				}
+			}
+		}
+	}
+
+	if !progress {
+		flow := 0
+		for id, minute := range open {
+			flow += valves[id].flow * (limit - minute)
+		}
+		return flow
+	}
+
+	return max
+}
+
+func stateHash(locationMe string, locationElephant string, minutesMe int, minutesElephant int, open map[string]int) string {
 	var ids []string
 	for id, _ := range open {
 		ids = append(ids, id)
@@ -78,7 +151,13 @@ func stateHash(location string, minutes int, open map[string]int) string {
 
 	sort.Strings(ids)
 
-	hash := location + fmt.Sprint(minutes)
+	var hash string
+	if locationMe < locationElephant {
+		hash = locationMe + fmt.Sprint(minutesMe) + locationElephant + fmt.Sprint(minutesElephant)
+	} else {
+		hash = locationElephant + fmt.Sprint(minutesElephant) + locationMe + fmt.Sprint(minutesMe)
+	}
+
 	for _, id := range ids {
 		hash += id + fmt.Sprint(open[id])
 	}
@@ -86,52 +165,107 @@ func stateHash(location string, minutes int, open map[string]int) string {
 	return hash
 }
 
-func findMaxFlow(valves map[string]valve, location string, open map[string]int, minutes int, memo map[string]int) int {
-	m, ok := memo[stateHash(location, minutes, open)]
+func findMaxFlow2(valves map[string]valve, ids []string, locationMe string, locationElephant string, open map[string]int, minutesMe int, minutesElephant int, limit int, memo map[string]int) int {
+	hash := stateHash(locationMe, locationElephant, minutesMe, minutesElephant, open)
+	m, ok := memo[hash]
 	if ok {
 		return m
 	}
 
-	if minutes == 30 {
-		memo[stateHash(location, minutes, open)] = 0
-		return 0
-	}
-
 	max := 0
+	bothProgess := false
+	for i, nextMe := range ids {
+		progressMe := false
+		etaMe := 0
+		_, openedMe := open[nextMe]
+		if !openedMe && valves[nextMe].flow > 0 {
+			etaMe = minutesMe + valves[locationMe].distances[nextMe] + 1
+			if etaMe <= limit {
+				progressMe = true
+				open[nextMe] = etaMe
+			}
+		}
 
-	_, opened := open[location]
-	if !opened && valves[location].flow > 0 {
-		open[location] = minutes
-		max = findMaxFlow(valves, location, open, minutes+1, memo)
-		delete(open, location)
+		if !progressMe {
+			continue
+		}
+
+		for j, nextElephant := range ids {
+			if i == j {
+				continue
+			}
+
+			progressElephant := false
+			etaElephant := 0
+			_, openedElephant := open[nextElephant]
+			if !openedElephant && valves[nextElephant].flow > 0 {
+				etaElephant = minutesElephant + valves[locationElephant].distances[nextElephant] + 1
+				if etaElephant <= limit {
+					progressElephant = true
+					open[nextElephant] = etaElephant
+				}
+			}
+
+			if !progressElephant {
+				continue
+			}
+
+			bothProgess = true
+			flow := findMaxFlow2(valves, ids, nextMe, nextElephant, open, etaMe, etaElephant, limit, memo)
+			if flow > max {
+				max = flow
+			}
+
+			delete(open, nextElephant)
+		}
+
+		delete(open, nextMe)
 	}
 
-	for _, next := range valves[location].tunnels {
-		flow := findMaxFlow(valves, next, open, minutes+1, memo)
+	if !bothProgess {
+		flowMe := findMaxFlow(valves, locationMe, open, minutesMe, limit, nil)
+		flowElephant := findMaxFlow(valves, locationElephant, open, minutesElephant, limit, nil)
 
-		if flow > max {
-			max = flow
+		if flowMe > max {
+			max = flowMe
+		}
+		if flowElephant > max {
+			max = flowElephant
 		}
 	}
 
-	for id, _ := range open {
-		max += valves[id].flow
-	}
-
-	memo[stateHash(location, minutes, open)] = max
+	memo[hash] = max
 	return max
 }
 
 func part1(valves map[string]valve) int {
+	valves = mapNetwork(valves)
+
 	open := make(map[string]int)
 	location := "AA"
 	memo := make(map[string]int)
 
-	return findMaxFlow(valves, location, open, 0, memo)
+	flow := findMaxFlow(valves, location, open, 0, 30, memo)
+	return flow
 }
 
 func part2(valves map[string]valve) int {
-	return 0
+	valves = mapNetwork(valves)
+
+	ids := make([]string, len(valves))
+	i := 0
+	for id, _ := range valves {
+		ids[i] = id
+		i++
+	}
+
+	open := make(map[string]int)
+	locationMe := "AA"
+	locationElephant := "AA"
+	memo := make(map[string]int)
+
+	flow := findMaxFlow2(valves, ids, locationMe, locationElephant, open, 0, 0, 26, memo)
+	return flow
 }
 
 func main() {
